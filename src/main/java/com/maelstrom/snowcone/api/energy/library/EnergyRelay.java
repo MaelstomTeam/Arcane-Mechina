9fcd3b5103371cea5c95b96e9b2e88e1819daae3
hybolic <hybolic3@hotmail.com>
2016-08-29 17:48:50 +1000

    Update

--- src/main/java/com/maelstrom/snowcone/api/energy/library/EnergyRelay.java
+++
@@ -1,9 +0,0 @@
-package com.maelstrom.snowcone.api.energy.library;
-
-import net.minecraft.tileentity.TileEntity;
-
-public interface EnergyRelay extends IEnergyBase
-{
-	public TileEntity[] getParents();
-	public TileEntity[] getChildern();
-}
